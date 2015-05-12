eb024aa895b9ff268e3aaed413c5977ba7591ef9
Peter Haddad <peterhad313@gmail.com>
2015-05-12 16:58:23 -0500

    complete steps 1 and 2. Creates Pickles

--- Assignment4/bayes_template.py
+++ Assignment4/bayes_template.py
@@ -7,22 +7,28 @@
 import math, os, pickle, re
 
 class Bayes_Classifier:
-
-   def __init__(self):
+    goodDict=dict()
+    badDict=dict()
+    def __init__(self):
       """This method initializes and trains the Naive Bayes Sentiment Classifier.  If a 
       cache of a trained classifier has been stored, it loads this cache.  Otherwise, 
       the system will proceed through training.  After running this method, the classifier 
       is ready to classify input text."""
+      if (not os.path.isfile("goodDict.pickle") or not os.path.isfile("badDict.pickle")):
+          self.train()
+      else:
+          self.goodDict=self.load("goodDict.pickle")
+          self.badDict=self.load("badDict.pickle")
 
-   def train(self):   
+    def train(self):   
       """Trains the Naive Bayes Sentiment Classifier."""
     
-   def classify(self, sText):
+    def classify(self, sText):
       """Given a target string sText, this function returns the most likely document
       class to which the target string belongs (i.e., positive, negative or neutral).
       """
 
-   def loadFile(self, sFilename):
+    def loadFile(self, sFilename):
       """Given a file name, return the contents of the file as a string."""
 
       f = open(sFilename, "r")
@@ -30,7 +36,7 @@ class Bayes_Classifier:
       f.close()
       return sTxt
    
-   def save(self, dObj, sFilename):
+    def save(self, dObj, sFilename):
       """Given an object and a file name, write the object to the file using pickle."""
 
       f = open(sFilename, "w")
@@ -38,7 +44,7 @@ class Bayes_Classifier:
       p.dump(dObj)
       f.close()
    
-   def load(self, sFilename):
+    def load(self, sFilename):
       """Given a file name, load and return the object stored in the file."""
 
       f = open(sFilename, "r")
@@ -47,7 +53,7 @@ class Bayes_Classifier:
       f.close()
       return dObj
 
-   def tokenize(self, sText): 
+    def tokenize(self, sText): 
       """Given a string of text sText, returns a list of the individual tokens that 
       occur in that string (in order)."""
 
@@ -67,3 +73,20 @@ class Bayes_Classifier:
          lTokens.append(sToken)
 
       return lTokens
+
+    def train(self):
+        for files in os.walk("./movies_reviews"):
+            for fileNames in files[2]:                
+                if fileNames[7]=='1':
+                    currDict=self.badDict
+                else:
+                    currDict=self.goodDict
+                tokens=self.tokenize(self.loadFile("./movies_reviews/"+fileNames))
+                for token in tokens:
+                    if token in currDict:
+                        currDict[token]+=1
+                    else:
+                        currDict[token]=1
+        self.save(self.goodDict, "goodDict.pickle")
+        self.save(self.badDict, "badDict.pickle")
+            
