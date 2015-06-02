d60fedf4bdd8eba1f31089d659174e46a7037e58
Peter Haddad <peterhad313@gmail.com>
2015-06-02 17:20:02 -0500

    Forgot to push final for HW4

--- Assignment4/test.py
+++ Assignment4/test.py
@@ -4,11 +4,7 @@ Created on Tue May 12 16:04:49 2015
 
 @author: peter
 """
-from bayes_template import *
+from hra069_kmg381_pbh423 import *
 
 b=Bayes_Classifier(0)
 b.validate()
-temp=b.tokenize("test 1 2 . ? the ",1)
-print b.classify("good awsome amazing")
-print b.classify("bad horrible aweful")
-print b.classify("the movie was ok")
